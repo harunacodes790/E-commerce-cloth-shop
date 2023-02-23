aeb821ec0888e7426a805989a168035e8c26f242
mohammed idris haruna <118879828+harunacodes790@users.noreply.github.com>
2023-02-23 07:35:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,20 @@ function ask(question, yes, no) {
   }
   
   ask("Do you want to buy cloth?", showOk, showCancel);
+
+let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
